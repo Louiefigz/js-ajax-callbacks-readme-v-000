fc77d0b2c55da0b3dcc7db3f1dc6b503f950d166
Louiefigz <figueroa.aaron@gmail.com>
2016-04-27 09:11:01 -0400

    Done.

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // We should wait for the page to load before running our ajax request
 $(document).ready(function(){
   // Now we start the Ajax Get request. The first parameter is the URL with the data.
-  // The second parameter is a function that handles the response.
-  $.get("html/sentence.html", function(response) {
-    // Here we are getting the element on the page with the id of sentences and
-    // insert the response
-    $("#sentences").html(response);
+  // // The second parameter is a function that handles the response.
+  // $.get("html/sentence.html", function(response) {
+  //   // Here we are getting the element on the page with the id of sentences and
+  //   // insert the response
+  //   $("#sentences").html(response);
+  // });
+
+
+  var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
+
+  $.get(url, function(response){
+      $("#sentences").html(response);
+    }).done(function(data) {
+      alert("Done");
+      console.log(data);
+    }).fail(function(error){
+      console.log("Something went wrong: " + error);
+      $("#sentences").html("Something went wrong: " + error);
+    });
   });
-});
